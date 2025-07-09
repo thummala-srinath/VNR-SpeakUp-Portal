af60938e701ad2b4d1bb4fa0ca1425fcf1f075e3
THUMMALA SRINATH <thummalasrinath46@gmail.com>
2025-07-09 11:46:53 +0530

    Update and rename script.js to

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-const countDocsByStatus = async (status) => {
-  const q = query(collection(db, "suggestions"), where("status", "==", status));
-  const snapshot = await getDocs(q);
-  return snapshot.size;
-};
+
