16a71b8b99b1de42f9465551d128657832bf2303
THUMMALA SRINATH <thummalasrinath46@gmail.com>
2025-07-10 18:26:57 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,17 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCb6Y5K32Cp9Zjke9mrXjJyM4nsg2x48X0",
   authDomain: "vnr-speakup-portal-17d1a.firebaseapp.com",
   projectId: "vnr-speakup-portal-17d1a",
-  storageBucket: "vnr-speakup-portal-17d1a.firebasestorage.app",
+  storageBucket: "vnr-speakup-portal-17d1a.appspot.com",  // ✅ fixed
   messagingSenderId: "5299604231",
-  appId: "1:5299604231:web:6a2044b03709cd492293a2",
-  measurementId: "G-TB0K252MX8"
+  appId: "1:5299604231:web:857b6efb82f6079f2293a2",
+  measurementId: "G-62SCFBLKQ7"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
